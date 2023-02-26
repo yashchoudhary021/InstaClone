838b2406e08b4b42d86b9fdce6b41f8a8b7cb124
Yash choudhary <86943555+yashchoudhary021@users.noreply.github.com>
2023-02-26 15:02:40 +0530

    Update Form.js

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import Navigation from "./Navigation";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Form() {
-    
+    const navigate = useNavigate();
     const [image, setImage] = useState(null);
     const [name, setName] = useState("");
     const [location, setLocation] = useState("");
     const [description, setDescription] = useState("");
 
-    const SubmitHandler = async (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         try {
             const formData = new FormData();
@@ -17,11 +18,13 @@ function Form() {
             formData.append("name", name);
             formData.append("location", location);
             formData.append("description", description);
-            const res = await axios.post("https://yash-instaclone-backend.onrender.com/upload", formData);            
+            const res = await axios.post("https://yash-instaclone-backend.onrender.com/upload", formData);
+           
             console.log(res.data);
         } catch (error) {
             console.error(error);
         }
+        navigate("/postview")
     };
 
     const handleImageChange = (e) => {
@@ -29,20 +32,19 @@ function Form() {
       };
     return (<>
         <Navigation />
-        <form action="/postview" onSubmit={SubmitHandler}>
+        <form  action="/postview" method="POST" onSubmit={handleSubmit}>
             <div id='first'>
                 <input type="file" onChange={handleImageChange} />
             </div>
             <div>
-                <input id='kkd' type="text" placeholder="Author" onChange={(e)=>setName(e.target.value)} required />
-                <input type="text" placeholder="Location" onChange={(e)=>setLocation(e.target.value)} required/>
+                <input id='kkd' type="text" placeholder="Author" onChange={(e)=>setName(e.target.value)} />
+                <input type="text" placeholder="Location" onChange={(e)=>setLocation(e.target.value)}/>
             </div>
             <div>
-                <input id='dis' type="text" placeholder="Description" onChange={(e)=>setDescription(e.target.value)} required />
+                <input id='dis' type="text" placeholder="Description" onChange={(e)=>setDescription(e.target.value)} />
             </div>
             <button id='submit' type="submit" >Post</button>
         </form>
     </>)
 }
 export default Form;
-
